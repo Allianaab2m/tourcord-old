3289633b7a1ec54e1b1aa38a4fd6b1ea8b16c559
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2021-12-25 18:11:33 +0900

    perf(files): 未使用のファイルを削除
    データベースは触らないので削除

--- src/utils/database.ts
+++
@@ -1,6 +0,0 @@
-import * as mysql from 'mysql2/promise'
-
-export const createConnnection = async (config: mysql.ConnectionOptions) => {
-    const connection = await mysql.createConnection(config)
-    return connection
-}
