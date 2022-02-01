df6ba1e3e86b37bc65ddad007be2b034d85395d5
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2022-02-01 13:19:49 +0900

    feat(bot): use Sapphire framework
    BREAKING CHANGE: D.js -> Sapphire

--- src/commands/ping.ts
+++
@@ -1,8 +0,0 @@
-import * as discord from 'discord.js'
-import { Bot } from '../class/bot'
-
-export const aliases = ['p', 'pg']
-
-export const ping = async (bot: Bot, message: discord.Message) => {
-    message.channel.send('pong')
-}
